c6e730c5fcb069e188becd46e4a9a89d765fb47e
AbhijeetJ512 <01fe21bei045@kletech.ac.in>
2023-04-18 08:26:49 +0530

    small changes

--- pattern_in_file.c
+++ pattern_in_file.c
@@ -18,7 +18,7 @@ void check(FILE *fptr,char str[100])
     int i=0,j=0,p1=0,status=1;
     while(!feof(fptr))
     {
-        fgets(lin,100,fptr);
+        fgets(lin,50,fptr);
         printf("%s",lin);
         int n1=count(lin);       
         int n2=count(str);
@@ -26,29 +26,20 @@ void check(FILE *fptr,char str[100])
         {
             for(j=0;j<n2;j++)
             {
-                printf("%c %c\n",lin[i+j],str[j]);
-                if(lin[i+j]!=str[j])
+                if(str[j]!=lin[i+j])
                 {
                     status=0;
-                    printf("next letter\n");
-                    break;
-                }
-                printf("%d\n",status);
-                if(str[j]=='\0')
-                {
-                    printf("Yes");
                     break;
                 }
             }
             if(j==n2&&status==1)
             {
                p1=1;
-               printf("it is checked");
                break;
             }
             status=1;
-
         }
+        status=1;
        if(p1==1)
        {
             printf("%s\n",lin);
