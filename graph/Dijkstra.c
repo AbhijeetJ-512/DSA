066dc7dfe7daffcaa3b372e3ee10cb7706917946
ABHIJEET J <97247727+AbhijeetJ512@users.noreply.github.com>
2023-07-17 12:00:08 +0530

    Update Dijkstra.c

--- graph/Dijkstra.c
+++ graph/Dijkstra.c
@@ -1,20 +1,10 @@
-// C program for Dijkstra's single source shortest path
-// algorithm. The program is for adjacency matrix
-// representation of the graph
-
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
-
-// Number of vertices in the graph
 #define V 6
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
 int minDistance(int dist[], bool sptSet[])
 {
-    // Initialize min value
     int min = INT_MAX, min_index;
 
     for (int v = 0; v < V; v++)
@@ -23,37 +13,25 @@ int minDistance(int dist[], bool sptSet[])
     return min_index;
 }
 
-// A utility function to print the constructed distance
-// array
 void printSolution(int dist[])
 {
     printf("Vertex \t\t Distance from Source\n");
     for (int i = 0; i < V; i++)
         printf("%d \t\t\t\t %d\n", i, dist[i]);
 }
-
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
 void dijkstra(int graph[V][V], int src)
 {
-    int dist[V]; // The output array. dist[i] will hold the
-                 // shortest
-    // distance from src to i
+    int dist[V]; 
 
     bool sptSet[V];
     for (int i = 0; i < V; i++)
         dist[i] = INT_MAX, sptSet[i] = false;
 
-    // Distance of source vertex from itself is always 0
     dist[src] = 0;
 
-    // Find shortest path for all vertices
     for (int count = 0; count < V - 1; count++)
     {
         int u = minDistance(dist, sptSet);
-
-        // Mark the picked vertex as processed
         sptSet[u] = true;
 
         for (int v = 0; v < V; v++)
